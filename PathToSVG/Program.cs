66df5522ff36d255e3e9da376f04a68a02d4ad61
Elias Dellago <ge36wuf@tum.de>
2026-04-04 00:56:13 +0200

    improve visuals

--- PathToSVG/Program.cs
+++ PathToSVG/Program.cs
@@ -27,7 +27,7 @@ Path3D test3D = new(Pieces: [
     new Line3D(new(len,0,arcRadius), new(len,0,len- arcRadius)),
     new Arc3D(Start: new(len,0,len- arcRadius), End: new(len, arcRadius, len), Center: new(len, arcRadius, len-arcRadius), Axis: rightVec, SweepDeg: 90),
     new Line3D(new(len,arcRadius,len), new(len,len-arcRadius,len)),
-    new Arc3D(Start: new(len,len - arcRadius,len), End: new(len, len, len-arcRadius), Center: new(len, len-arcRadius, len-arcRadius), Axis: rightVec, SweepDeg: 470),
+    new Arc3D(Start: new(len,len - arcRadius,len), End: new(len, len, len-arcRadius), Center: new(len, len-arcRadius, len-arcRadius), Axis: rightVec, SweepDeg: 90),
     new Line3D(new(len,len,len-arcRadius), new(len,len,arcRadius)),
     new Arc3D(Start: new(len,len, arcRadius), End: new(len+arcRadius, len, 0), Center: new(len+arcRadius, len, arcRadius), Axis: inVec, SweepDeg: 90),
     new Line3D(new(len+arcRadius, len, 0), new(len*2,len,0)),
@@ -64,7 +64,9 @@ var degreesPerSample = 5.0f;
 
 var preferLongestLine = true;
 
-var path = testCage;
+var debugBounds = true;
+
+var path = test3D;
 
 var imagePath = path.ToImagePath(view, preferLongestLine, degreesPerSample);
 
@@ -103,14 +105,12 @@ using (var canvas = SKSvgCanvas.Create(canvasRect, stream))
 {
     using var backgroundPaint = new SKPaint
     {
-        Color = new SKColor(50, 50, 80),
-        Style = SKPaintStyle.Fill
+        Color = new SKColor(100, 100, 130),
     };
 
     using var marginPaint = new SKPaint
     {
-        Color = new SKColor(100, 100, 130),
-        IsAntialias = true
+        Color = new SKColor(50, 50, 80),
     };
 
     using var linePaint = new SKPaint
@@ -134,63 +134,38 @@ using (var canvas = SKSvgCanvas.Create(canvasRect, stream))
         IsAntialias = true
     };
 
-    #region Draw Backgrounds
     canvas.DrawRect(canvasRect, backgroundPaint);
-    canvas.DrawRect(margin.Left, margin.Top, bounds.Range.X, bounds.Range.Y, marginPaint);
-    #endregion
-
-    #region Draw Measurements
-    var halfStrokeWidth = measurePaint.StrokeWidth * 0.5f;
-    var capWidth = measurePaint.StrokeWidth * 4f;
-    var halfCapWidth = capWidth * 0.5f;
-
-    var fullWidth = margin.Left + bounds.Range.X + margin.Right;
-    var fullHeight = margin.Top + bounds.Range.Y + margin.Bottom;
-
-    var xLineHeight = margin.Top + bounds.Range.Y + halfCapWidth + measurePaint.StrokeWidth;
-    var yLineWidth = margin.Left + bounds.Range.X + halfCapWidth + measurePaint.StrokeWidth;
-
-    if (bounds.Range.X > imagePath.Diameter)
-    {
-        var rangeXStart = new SKPoint(margin.Left, xLineHeight);
-        var rangeXEnd = new SKPoint(margin.Left + bounds.Range.X, xLineHeight);
-        canvas.DrawLine(rangeXStart, rangeXEnd, measurePaint);
-
-        canvas.DrawLine(rangeXStart + new SKPoint(halfStrokeWidth, halfCapWidth), rangeXStart + new SKPoint(halfStrokeWidth, -halfCapWidth), measurePaint);
-        canvas.DrawLine(rangeXEnd + new SKPoint(-halfStrokeWidth, halfCapWidth), rangeXEnd + new SKPoint(-halfStrokeWidth, -halfCapWidth), measurePaint);
-
-        canvas.DrawText($"X: {bounds.Range.X}", bounds.Range.X * 0.5f + margin.Left, fullHeight - measurePaint.StrokeWidth, SKTextAlign.Center, textFont, measurePaint);
-    }
-
-    if (bounds.Range.Y > imagePath.Diameter)
-    {
-        var rangeYStart = new SKPoint(yLineWidth, margin.Top);
-        var rangeYEnd = new SKPoint(yLineWidth, margin.Top + bounds.Range.Y);
-        canvas.DrawLine(rangeYStart, rangeYEnd, measurePaint);
-        canvas.DrawLine(rangeYStart + new SKPoint(halfCapWidth, halfStrokeWidth), rangeYStart + new SKPoint(-halfCapWidth, halfStrokeWidth), measurePaint);
-        canvas.DrawLine(rangeYEnd + new SKPoint(halfCapWidth, -halfStrokeWidth), rangeYEnd + new SKPoint(-halfCapWidth, -halfStrokeWidth), measurePaint);
-
-        canvas.DrawText($"Y: {bounds.Range.Y}", fullWidth - measurePaint.StrokeWidth, textFont.Size + measurePaint.StrokeWidth, SKTextAlign.Right, textFont, measurePaint);
-    }
-
-    if (bounds.Range.Z > imagePath.Diameter)
-    {
-        var markerCenter = new SKPoint(yLineWidth, xLineHeight);
-        canvas.DrawCircle(markerCenter, radius: halfCapWidth, measurePaint);
-        canvas.DrawCircle(markerCenter, radius: halfCapWidth - measurePaint.StrokeWidth, backgroundPaint);
-
-        canvas.DrawText($"Z: {bounds.Range.Z}", fullWidth - measurePaint.StrokeWidth, fullHeight - measurePaint.StrokeWidth, SKTextAlign.Right, textFont, measurePaint);
-    }
-    #endregion
 
     #region Draw ImagePath
+    //Arcs first
+    for (int pieceIdx = 0; pieceIdx < imagePath.Pieces.Count; pieceIdx++)
+    {
+        var piece = imagePath.Pieces[pieceIdx];
+        if (piece is ImageArc arc)
+        {
+            for (int sampleIdx = 0; sampleIdx < arc.ImageSweepSamples.Count - 1; sampleIdx++)
+            {
+                var start = arc.ImageSweepSamples[sampleIdx];
+                var end = arc.ImageSweepSamples[sampleIdx + 1];
+                canvas.DrawLine(start.ToSKPoint(bounds, margin), end.ToSKPoint(bounds, margin), arcPaint);
+            }
+        }
+    }
+    //Then lines
     for (int pieceIdx = 0; pieceIdx < imagePath.Pieces.Count; pieceIdx++)
     {
         var piece = imagePath.Pieces[pieceIdx];
         if (piece is ImageLine line)
         {
             canvas.DrawLine(line.ImageStart.ToSKPoint(bounds, margin), line.ImageEnd.ToSKPoint(bounds, margin), linePaint);
-
+        }
+    }
+    //Text labels at the end
+    for (int pieceIdx = 0; pieceIdx < imagePath.Pieces.Count; pieceIdx++)
+    {
+        var piece = imagePath.Pieces[pieceIdx];
+        if (piece is ImageLine line)
+        {
             if (imagePath.Pieces.Count > 1)
             {
                 var imageCenter = line.ImageStart + (line.ImageEnd - line.ImageStart) * 0.5f;
@@ -207,29 +182,83 @@ using (var canvas = SKSvgCanvas.Create(canvasRect, stream))
                 var heightOffset = distToTop < distToCenterHeight ? textFont.Size : (distToBottom < distToCenterHeight ? 0 : textFont.Size * 0.5f);
 
                 var textAlign = distToLeft < distToCenterWidth ? SKTextAlign.Left : (distToRight < distToCenterWidth ? SKTextAlign.Right : SKTextAlign.Center);
-                canvas.DrawText(lineString, imageCenter.ToSKPoint(bounds, margin) + new SKPoint(0, heightOffset), textAlign, textFont, arcPaint);
+                canvas.DrawText(lineString, imageCenter.ToSKPoint(bounds, margin) + new SKPoint(0, heightOffset), textAlign, textFont, measurePaint);
             }
         }
         else if (piece is ImageArc arc)
         {
-            for (int sampleIdx = 0; sampleIdx < arc.ImageSweepSamples.Count - 1; sampleIdx++)
-            {
-                var start = arc.ImageSweepSamples[sampleIdx];
-                var end = arc.ImageSweepSamples[sampleIdx + 1];
-                canvas.DrawLine(start.ToSKPoint(bounds, margin), end.ToSKPoint(bounds, margin), arcPaint);
-            }
+            var distToTop = Math.Abs(arc.ImageCenter.Y - bounds.Max.Y);
+            var distToBottom = Math.Abs(arc.ImageCenter.Y - bounds.Min.Y);
+            var distToCenterHeight = Math.Abs(arc.ImageCenter.Y - bounds.Center.Y);
+
+            var heightOffset = distToTop < distToCenterHeight ? textFont.Size : (distToBottom < distToCenterHeight ? 0 : textFont.Size * 0.5f);
 
             var angleString = $"{Math.Abs(arc.Arc3D.SweepDeg).ToString("0.#")}°";
-            canvas.DrawText(angleString, arc.ImageCenter.ToSKPoint(bounds, margin) + new SKPoint(0, textFont.Size * 0.5f), SKTextAlign.Center, textFont, linePaint);
+            canvas.DrawText(angleString, arc.ImageCenter.ToSKPoint(bounds, margin) + new SKPoint(0, heightOffset), SKTextAlign.Center, textFont, measurePaint);
         }
     }
     #endregion
 
-    #region Draw ImagePath Bounds (Debug)
-    foreach (var outerPt in imagePath.Pieces.SelectMany(x => x.ImageBounds))
+    var fullWidth = margin.Left + bounds.Range.X + margin.Right;
+    var fullHeight = margin.Top + bounds.Range.Y + margin.Bottom;
+
+    #region Draw Margins (possibly overpaints ImagePath)
+    canvas.DrawRect(0, 0, fullWidth, margin.Top, marginPaint);
+    canvas.DrawRect(0, fullHeight - margin.Bottom, fullWidth, margin.Bottom, marginPaint);
+    canvas.DrawRect(0, 0, margin.Left, fullHeight, marginPaint);
+    canvas.DrawRect(fullWidth - margin.Right, 0, margin.Right, fullHeight, marginPaint);
+
+    #endregion
+
+    #region Draw Measurements
+    var halfStrokeWidth = measurePaint.StrokeWidth * 0.5f;
+    var capWidth = measurePaint.StrokeWidth * 4f;
+    var halfCapWidth = capWidth * 0.5f;
+
+    var xLineHeight = margin.Top + bounds.Range.Y + halfCapWidth + measurePaint.StrokeWidth;
+    var yLineWidth = margin.Left + bounds.Range.X + halfCapWidth + measurePaint.StrokeWidth;
+
+    if (bounds.Range.X > imagePath.Diameter)
     {
-        var pt = outerPt.ToSKPoint(bounds, margin);
-        canvas.DrawCircle(pt, radius: 1, markerPaint);
+        var rangeXStart = new SKPoint(margin.Left, xLineHeight);
+        var rangeXEnd = new SKPoint(margin.Left + bounds.Range.X, xLineHeight);
+        canvas.DrawLine(rangeXStart, rangeXEnd, measurePaint);
+
+        canvas.DrawLine(rangeXStart + new SKPoint(halfStrokeWidth, halfCapWidth), rangeXStart + new SKPoint(halfStrokeWidth, -halfCapWidth), measurePaint);
+        canvas.DrawLine(rangeXEnd + new SKPoint(-halfStrokeWidth, halfCapWidth), rangeXEnd + new SKPoint(-halfStrokeWidth, -halfCapWidth), measurePaint);
+
+        canvas.DrawText($"X: {bounds.Range.X.ToString("0.#")}", bounds.Range.X * 0.5f + margin.Left, fullHeight - measurePaint.StrokeWidth, SKTextAlign.Center, textFont, measurePaint);
+    }
+
+    if (bounds.Range.Y > imagePath.Diameter)
+    {
+        var rangeYStart = new SKPoint(yLineWidth, margin.Top);
+        var rangeYEnd = new SKPoint(yLineWidth, margin.Top + bounds.Range.Y);
+        canvas.DrawLine(rangeYStart, rangeYEnd, measurePaint);
+        canvas.DrawLine(rangeYStart + new SKPoint(halfCapWidth, halfStrokeWidth), rangeYStart + new SKPoint(-halfCapWidth, halfStrokeWidth), measurePaint);
+        canvas.DrawLine(rangeYEnd + new SKPoint(halfCapWidth, -halfStrokeWidth), rangeYEnd + new SKPoint(-halfCapWidth, -halfStrokeWidth), measurePaint);
+
+        canvas.DrawText($"Y: {bounds.Range.Y.ToString("0.#")}", fullWidth - measurePaint.StrokeWidth, textFont.Size + measurePaint.StrokeWidth, SKTextAlign.Right, textFont, measurePaint);
+    }
+
+    if (bounds.Range.Z > imagePath.Diameter)
+    {
+        var markerCenter = new SKPoint(yLineWidth, xLineHeight);
+        canvas.DrawCircle(markerCenter, radius: halfCapWidth, measurePaint);
+        canvas.DrawCircle(markerCenter, radius: halfCapWidth - measurePaint.StrokeWidth, marginPaint);
+
+        canvas.DrawText($"Z: {bounds.Range.Z.ToString("0.#")}", fullWidth - measurePaint.StrokeWidth, fullHeight - measurePaint.StrokeWidth, SKTextAlign.Right, textFont, measurePaint);
+    }
+    #endregion
+
+    #region Draw ImagePath Bounds (Debug)
+    if (debugBounds)
+    {
+        foreach (var outerPt in imagePath.Pieces.SelectMany(x => x.ImageBounds))
+        {
+            var pt = outerPt.ToSKPoint(bounds, margin);
+            canvas.DrawCircle(pt, radius: 1, markerPaint);
+        }
     }
     #endregion
 }
